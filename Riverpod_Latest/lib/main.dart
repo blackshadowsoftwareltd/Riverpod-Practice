cee09bbbcfde125712e59e127035980083494707
remon <blackshadowsoftwareltd@gmail.com>
2023-02-08 23:54:29 +0600

    provider in home

--- Riverpod_Latest/lib/main.dart
+++ Riverpod_Latest/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart' show ProviderScope;
+import 'package:flutter_riverpod/flutter_riverpod.dart'
+    show ConsumerWidget, ProviderScope, WidgetRef;
 
 void main() => runApp(const ProviderScope(child: MaterialApp(home: Home())));
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(appBar: AppBar(title: const Text('Riverpod Latest')));
   }
 }
