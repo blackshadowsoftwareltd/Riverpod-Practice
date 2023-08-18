2705094f1d215e1c1a2dbbf4c8ab866785e8431a
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-08-18 20:09:49 +0600

    code generator init

---
+++ riverpod_code_generator/README.md
@@ -0,0 +1,9 @@
+# riverpod_code_generator
+
+```
+dart pub run build_runner watch
+```
+
+```
+dart pub run build_runner watch --delete-conflicting-outputs
+```
---
+++ riverpod_code_generator/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'modules/home.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MaterialApp(home: Home())));
+}
